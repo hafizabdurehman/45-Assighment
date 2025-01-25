553966c4fa4e1728b77200a001d80af8bd8184bb
hafiz abdur rehman <hafizmuhammadabdurrehman7@gmail.com>
2025-01-25 19:48:07 +0500

    Add files via upload

---
+++ 45 assighment/1  to 9 question/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//==  = =let personalname="abdur rehman ";
+//console.log ('hello personname "would you like to leatn some typscript toda');
+let personname = "Abdur rehman";
+console.log(pesonName.tollowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toupperCase() + personName.slice(1).tolowerCase());
---
+++ 45 assighment/1  to 9 question/main.ts
@@ -0,0 +1,6 @@
+//let personName="abdur rehman";
+//console.log('hello'; personName;"would you like to learn some Typscript today");
+let personName="Abdur rehman"
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
