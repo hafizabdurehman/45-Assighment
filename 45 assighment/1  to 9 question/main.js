c4e474cb2df49c8064c2ddc220461e23d3bc0217
hafiz abdur rehman <hafizmuhammadabdurrehman7@gmail.com>
2025-01-30 20:18:04 +0500

    Delete 45 assighment/1  to 9 question directory

--- 45 assighment/1  to 9 question/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//==  = =let personalname="abdur rehman ";
-//console.log ('hello personname "would you like to leatn some typscript toda');
-let personname = "Abdur rehman";
-console.log(pesonName.tollowerCase());
-console.log(personName.toUpperCase());
-console.log(personName.charAt(0).toupperCase() + personName.slice(1).tolowerCase());
--- 45 assighment/1  to 9 question/main.ts
+++
@@ -1,6 +0,0 @@
-//let personName="abdur rehman";
-//console.log('hello'; personName;"would you like to learn some Typscript today");
-let personName="Abdur rehman"
-console.log(personName.toLowerCase());
-console.log(personName.toUpperCase());
-console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
